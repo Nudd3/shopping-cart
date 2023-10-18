ad2762a2729953c17dcc784faf245a1b1a154b96
Noak Pettersson <noake12@gmail.com>
2023-10-18 20:07:10 +0200

    header layout for hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,21 +7,13 @@ const Header = () => {
       <div className='header-title'>
         <h1>Magnation</h1>
       </div>
-      <nav className='header-nav'>
-        {/* <input type="checkbox" style={{display:'none'}} className=''/> */}
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul className='header-nav-list'>
-          <li className='header-nav-list-item'>
-            <a href=''>About</a>
-          </li>
-          <li className='header-nav-list-item'>
-            <a href=''>Contact</a>
-          </li>
-          <li className='header-nav-list-item'>
-            <a href=''>Store</a>
-          </li>
+      <nav className='navigation'>
+        <input type="checkbox" className='toggle-menu'/>
+        <div className='hamburger'></div>
+        <ul className='menu'>
+          <li className='header-nav-list-item'><a href=''>About</a></li>
+          <li className='header-nav-list-item'><a href=''>Contact</a></li>
+          <li className='header-nav-list-item'><a href=''>Store</a></li>
         </ul>
       </nav>
       <CartIcon />
