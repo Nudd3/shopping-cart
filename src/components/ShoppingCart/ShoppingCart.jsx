bed1aad554963e07d67f95ff86363d948e912e11
Noak Pettersson <noake12@gmail.com>
2023-11-12 16:37:55 +0100

    Shopping cart working

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,32 +1,89 @@
 import './ShoppingCart.scss';
 import { useContext } from 'react';
 import { ShopContext } from '../../App';
-
+import helperMethods from '../../utils/utils';
 const ShoppingCart = () => {
+  const { cartIsOpen, cartItems, storeItems, toggleCart, removeFromCart } =
+    useContext(ShopContext);
 
-  const {cartIsOpen, cartItems} = useContext(ShopContext);
-
-  const mainClass = cartIsOpen ? 'shopping-cart shopping-cart-open' : 'shopping-cart'
-  const background = cartIsOpen 
+  const mainClass = cartIsOpen
+    ? 'shopping-cart shopping-cart-open'
+    : 'shopping-cart';
+  const background = cartIsOpen
     ? 'shopping-cart-background shopping-cart-background-cover'
     : 'shopping-cart-background';
 
+  const renderCartItem = (item) => {
+    const itm = storeItems.find((storeItem) => item.id === storeItem.id);
+    return (
+      <div className='shopping-cart-item' key={item.id}>
+        <img className='shopping-cart-item-image' src={itm.image} alt='' />
+        <h2 className='shopping-cart-item-title'>
+          {helperMethods.formatTitle(itm.title)}
+        </h2>
+        <h2 className='shopping-cart-item-quantity'>{item.quantity}</h2>
+        <svg className="shopping-cart-item-trashcan" viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' onClick={() => removeFromCart(item.id)}>
+          <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+          <g
+            id='SVGRepo_tracerCarrier'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          ></g>
+          <g id='SVGRepo_iconCarrier'>
+            {' '}
+            <path
+              d='M9.87787 4.24993C10.1871 3.37503 11.0215 2.75 12 2.75C12.9785 2.75 13.813 3.37503 14.1222 4.24993C14.2602 4.64047 14.6887 4.84517 15.0793 4.70713C15.4698 4.56909 15.6745 4.1406 15.5365 3.75007C15.022 2.29459 13.634 1.25 12 1.25C10.3661 1.25 8.97804 2.29459 8.46361 3.75007C8.32557 4.1406 8.53026 4.56909 8.9208 4.70713C9.31134 4.84517 9.73983 4.64047 9.87787 4.24993Z'
+              fill='#000'
+            ></path>{' '}
+            <path
+              d='M2.75 6C2.75 5.58579 3.08579 5.25 3.5 5.25H20.5001C20.9143 5.25 21.2501 5.58579 21.2501 6C21.2501 6.41421 20.9143 6.75 20.5001 6.75H3.5C3.08579 6.75 2.75 6.41421 2.75 6Z'
+              fill='#000'
+            ></path>{' '}
+            <path
+              d='M5.11686 7.75166C5.53015 7.72411 5.88753 8.03681 5.91508 8.45011L6.37503 15.3492C6.46488 16.6971 6.52891 17.6349 6.66948 18.3405C6.80583 19.025 6.99616 19.3873 7.26957 19.6431C7.54299 19.8988 7.91715 20.0647 8.60915 20.1552C9.32255 20.2485 10.2626 20.25 11.6134 20.25H12.3868C13.7376 20.25 14.6776 20.2485 15.391 20.1552C16.083 20.0647 16.4572 19.8988 16.7306 19.6431C17.004 19.3873 17.1943 19.025 17.3307 18.3405C17.4712 17.6349 17.5353 16.6971 17.6251 15.3492L18.0851 8.45011C18.1126 8.03681 18.47 7.72411 18.8833 7.75166C19.2966 7.77921 19.6093 8.13659 19.5818 8.54989L19.1183 15.5016C19.0328 16.7844 18.9637 17.8205 18.8018 18.6336C18.6334 19.4789 18.347 20.185 17.7554 20.7384C17.1638 21.2919 16.4402 21.5307 15.5856 21.6425C14.7635 21.75 13.7251 21.75 12.4395 21.75H11.5607C10.2751 21.75 9.23663 21.75 8.41459 21.6425C7.55994 21.5307 6.83639 21.2919 6.2448 20.7384C5.6532 20.185 5.36678 19.4789 5.19838 18.6336C5.03641 17.8205 4.96735 16.7844 4.88186 15.5016L4.41841 8.54989C4.39085 8.13659 4.70356 7.77921 5.11686 7.75166Z'
+              fill='#000'
+            ></path>{' '}
+          </g>
+        </svg>
+      </div>
+    );
+  };
+
   return (
     <>
-        <div className={background}>
-          <div className={mainClass}>
-            <div className="shopping-cart-header">
-              <h1 className='shopping-cart-title'>In your cart</h1>
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="4527a0"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.19 2H7.81C4.17 2 2 4.17 2 7.81V16.18C2 19.83 4.17 22 7.81 22H16.18C19.82 22 21.99 19.83 21.99 16.19V7.81C22 4.17 19.83 2 16.19 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#4527a0"></path> </g></svg>
-            </div>
-
+      <div className={background}>
+        <div className={mainClass}>
+          <div className='shopping-cart-header'>
+            <h1 className='shopping-cart-title'>In your cart</h1>
+            <svg
+              viewBox='0 0 24 24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              stroke='#fff'
+              onClick={toggleCart}
+            >
+              <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+              <g
+                id='SVGRepo_tracerCarrier'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              ></g>
+              <g id='SVGRepo_iconCarrier'>
+                {' '}
+                <path
+                  d='M16.19 2H7.81C4.17 2 2 4.17 2 7.81V16.18C2 19.83 4.17 22 7.81 22H16.18C19.82 22 21.99 19.83 21.99 16.19V7.81C22 4.17 19.83 2 16.19 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z'
+                  fill='#e03838'
+                ></path>{' '}
+              </g>
+            </svg>
+          </div>
+          <div className='shopping-cart-body'>
+            {cartItems.map((item) => renderCartItem(item))}
           </div>
         </div>
+      </div>
     </>
-  )
-
-}
-
-
+  );
+};
 
 export default ShoppingCart;
