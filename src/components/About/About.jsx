53839d57dba366ea5526f59304ee5cbc70856482
Noak Pettersson <noake12@gmail.com>
2023-11-30 23:45:57 +0100

    about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,14 @@ import './About.scss';
 const About = () => {
   return (
     <div className='bg-image bg-settings'>
-      <h1></h1>
+      <div className="about">
+        <div className="about-text">
+          <h1>About</h1>
+          <p>This project is created as a part of The Odin Project's Fullstack JavaScript curriculum</p>
+          <br></br>
+          <p>To see more of my stuff, check out my <a href="https://github.com/Nudd3">GitHub</a> account</p>
+        </div>
+      </div>
     </div>
   );
 };
